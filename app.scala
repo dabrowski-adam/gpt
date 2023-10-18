44699b0652173699b737218eef1d5bfb3a59f5eb
Adam Dąbrowski <contact@adamdabrowski.com>
2023-10-18 17:54:44 +0000

    Clean up

--- app.scala
+++ app.scala
@@ -4,5 +4,13 @@ import cats.effect.*
 
 val MESSAGE = "Hello toolkit!"
 
-object Hello extends IOApp.Simple:
-  def run = IO.println(MESSAGE)
+object App extends IOApp:
+
+  def hello = IO.pure(println("hello")).as(ExitCode.Success)
+
+  def run(args: List[String]): IO[ExitCode] =
+    hello.guaranteeCase:
+      case Outcome.Canceled() =>
+        IO(println("Interrupted: releasing and exiting!"))
+      case _ =>
+        IO(println("Normal exit!"))
--- project.scala
+++ project.scala
@@ -2,7 +2,8 @@
 // // > using platform scala-native
 // // > using nativeVersion 0.4.15
 
-//> using toolkit typelevel:0.1.17
+//> using dep org.typelevel::cats-core:2.10.0
+//> using dep org.typelevel::cats-effect:3.5.2
 
 //> using test.dep com.disneystreaming::weaver-cats:0.8.3
 //> using testFramework "weaver.framework.CatsEffect"
