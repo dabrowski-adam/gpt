67b2f34f9c7016df56dd2171e79b1e7d44cc9790
Adam Dąbrowski <contact@adamdabrowski.com>
2023-10-18 15:57:25 +0000

    Configure Scala Native

--- Cli.scala
+++ Cli.scala
@@ -1,2 +1,9 @@
-@main
-def hello() = println("Hello, world")
+//> using scala 3.3.0
+//> using platform scala-native
+//> using nativeVersion 0.4.15
+//> using toolkit typelevel:0.1.17
+
+import cats.effect.*
+
+object Hello extends IOApp.Simple:
+  def run = IO.println("Hello toolkit!")
